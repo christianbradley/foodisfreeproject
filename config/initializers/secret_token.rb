b950f63a56544ad00cb3aacea987e083822f99b5
Christian Bradley <christian.bradley@rackspace.com>
2012-01-31 13:57:42 -0600

    Post exec: 'rails new -T'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodisfreeproject::Application.config.secret_token = '886fd338c735f3c45394d414cc64ecb1b710f9169b0772e51087b65ed705df6482ceeae5287437cc04d47d815903bca07123a1410e84a29fc648dce510edaa03'
